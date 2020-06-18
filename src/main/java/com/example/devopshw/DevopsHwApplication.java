d1943cfbf07a18b3516b786817c8a965ee68d095
Felipe Duarte <felipeduartedreis@gmail.com>
2020-06-18 20:08:48 -0300

    initial commit

---
+++ src/main/java/com/example/devopshw/DevopsHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.devopshw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsHwApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsHwApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/devopshw/DevopsHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devopshw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsHwApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
